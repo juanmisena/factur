7a50b9525ebd7cf55d045b6b3993b0906192c208
juan <jdbetancourth22@misena.edu.co>
2022-03-26 14:17:20 -0500

    agregar nuevo ejercicio

--- sentencia.js
+++ sentencia.js
@@ -690,6 +690,6 @@ if (conEscudo == true) {
 }
 document.write('Gracias. Aquí tiene el desglose de su compra<br>');
 document.write('Bandera de %5d cm2: %5.2f €\n', anchura * altura, (anchura * altura / 100),'<br>');
-document.write('%s:           %5.2f €\n', escudoCadena, precioEscudo,'<br>');
+document.write('%s:           %5.2f €\n', escudoCadena, '&nbsp;',precioEscudo,'<br>');
 document.write('Gastos de envío:      %5.2f €\n', 3.25,'<br>');
 document.write('Total:                %5.2f €\n', (anchura * altura / 100) + precioEscudo + 3.25,'<br>');
