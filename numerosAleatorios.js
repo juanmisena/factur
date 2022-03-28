7394ec629cd5f0e03598dbee2d1a6b2dd9006541
juan <jdbetancourth22@misena.edu.co>
2022-03-28 16:59:03 -0500

    agregar nuevos ejercicios

--- numerosAleatorios.js
+++ numerosAleatorios.js
@@ -3,11 +3,11 @@
 // let suma = 0;
 // alert('Tirada de tres dados: ');
 // for (let i = 0; i < 3; i++) {
-//     tirada = (Math.random() * 6) + 1;
+//     tirada = (Math.round(Math.random() * 6)) + 1;
 //     document.write(tirada + ' ');
 //     suma += tirada;
 // }
-// document.write('<br>' + suma + ' ');
+// document.write(' ' + suma + ' ');
 
 //Ejercicio 2
 // let palo = '';
