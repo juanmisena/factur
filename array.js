d314dc9efcb04e4dc9f83312e5db01f0cf009a82
Juan'Da Betancourth <88244596+juanmisena@users.noreply.github.com>
2022-04-04 15:36:08 -0500

    agregar nuevos ejercicios

--- array.js
+++ array.js
@@ -1,12 +1,24 @@
 //Ejercicio 1
-var num = new Array(5);
-num[0] = 39;
-num[1] = -2;
-num[4] = 0;
-num[6] = 14;
-num[8] = 5;
-num[9] = 120;
-console.log(num);
-for (let i = 0; i < 12; i++) {
-    console.log('num[%2d]: %d\n', i, num[i]);
-}
+// var num = new Array(5);
+// num[0] = 39;
+// num[1] = -2;
+// num[4] = 0;
+// num[6] = 14;
+// num[8] = 5;
+// num[9] = 120;
+// console.log(num);
+// for (let i = 0; i < 12; i++) {
+//     console.log('num[%2d]: %d\n', i, num[i]);
+// }
+
+//Ejercicio 2
+// var simbolo = new Array('10');
+// simbolo[0] = 'a';
+// simbolo[1] = 'x';
+// simbolo[4] = '@';
+// simbolo[6] = ' ';
+// simbolo[7] = '+';
+// simbolo[8] = 'Q';
+// for(let i = 0; i < 10; i++) {
+//   console.log('simbolo[' + i + ']: ' + simbolo[i]);
+// }
